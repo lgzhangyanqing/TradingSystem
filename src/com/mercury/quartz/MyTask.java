2d4140678688ba7e87d2d0d71e525ff94ea6392f
yanqing <yqjavaece@gmail.com>
2016-01-28 14:16:59 -0500

    add branch1 thing

--- src/com/mercury/quartz/MyTask.java
+++ src/com/mercury/quartz/MyTask.java
@@ -12,36 +12,35 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import com.mercury.service.TransService;
+
 /*
 @ComponentScan*/
 public class MyTask {
 
-	 @Autowired
-	 private TransService ts;
-	  
-	@Scheduled(fixedRate=60000)
-	 public void printCurrentTime() {  
-		  // printing current system time  
-		  System.out.println("Current Time : " + Calendar.getInstance().getTime());  
-		  List<Integer> tranList = new ArrayList<>();
-			FileReader fr;
-			try {
-				fr = new FileReader("C:\\Users\\royce\\git\\TradingSystem\\WebContent\\CSV\\pending.csv");
-			
-			 BufferedReader br = new BufferedReader (fr);
-			 String s = null;
-			 int count = 0;
-				while((s=br.readLine())!=null){
-					System.out.println(s);
-					tranList.add(count);
-					 count++;
-				 }
-			} catch (NumberFormatException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			 ts.commitPendings(tranList);
+	@Autowired
+	private TransService ts;
 
-		 }
-	
+	@Scheduled(fixedRate = 60000)
+	public void printCurrentTime() {
+		// printing current system time
+		System.out.println("Current Time : " + Calendar.getInstance().getTime());
+		List<Integer> tranList = new ArrayList<>();
+		
+		try(FileReader fr = new FileReader("C:\\Users\\royce\\git\\TradingSystem\\WebContent\\CSV\\pending.csv");
+			BufferedReader br = new BufferedReader(fr);){
+			String s = null;
+			//count means every tranID
+			int count = 0;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s);
+				tranList.add(count);
+				count++;
+			}
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		ts.commitPendings(tranList);
+
+	}
+
 }
