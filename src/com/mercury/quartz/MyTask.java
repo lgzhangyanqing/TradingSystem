d4133b43a2619098f9eb9deb221668fd8fc14f2f
YANG <YANG@10.0.0.137>
2016-01-31 11:00:57 -0500

    sdaz

---
+++ src/com/mercury/quartz/MyTask.java
@@ -0,0 +1,46 @@
+package com.mercury.quartz;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.mercury.service.TransService;
+
+/*
+@ComponentScan*/
+public class MyTask {
+
+	@Autowired
+	private TransService ts;
+
+	@Scheduled(fixedRate = 60000)
+	public void printCurrentTime() {
+		// printing current system time
+		System.out.println("Current Time : " + Calendar.getInstance().getTime());
+		List<Integer> tranList = new ArrayList<>();
+		
+		try(FileReader fr = new FileReader("C:\\Users\\YANG\\git\\TradingSystem\\WebContent\\CSV\\pending.csv");
+			BufferedReader br = new BufferedReader(fr);){
+			String s = null;
+			//count means every tranID
+			int count = 0;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s);
+				tranList.add(count);
+				count++;
+			}
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		ts.commitPendings(tranList);
+
+	}
+
+}
