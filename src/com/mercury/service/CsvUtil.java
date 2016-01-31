48040c41de18979ef5c59d479ee4128b51977e8d
YANG <YANG@10.0.0.137>
2016-01-31 10:59:39 -0500

    merged yang

--- src/com/mercury/service/CsvUtil.java
+++ src/com/mercury/service/CsvUtil.java
@@ -22,7 +22,9 @@ import com.mercury.dao.UserDao;
 
 @Service
 public class CsvUtil {
-	private final String CSV_PATH = "D:\\home\\git\\TradingSystem\\WebContent\\CSV\\pending.csv";
+
+	private final String CSV_PATH = "C:\\Users\\YANG\\git\\TradingSystem\\WebContent\\CSV\\pending.csv";
+
 	public String csvPath;
 	
 	@Autowired
